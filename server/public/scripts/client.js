791fb43c6b8a30526ada1eb84061ac2fffdb243a
Vada Karlen <vada.karlen@gmail.com>
2021-02-06 11:35:16 -0600

    Started sendEquation to take in the inputs. Added check to make sure values were given. Added clearCalculator function to clear all inputs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,23 +1,42 @@
-console.log('JS');
-
 $(document).ready(onReady);
 
 let operator = '';
 
 function onReady() {
-  console.log('JQ');
-
   // create button calls
   $('.operator').on('click', changeOperator);
-  $('#calcForm').on('submit', doCalculation);
-}
+  $('#calcForm').on('submit', sendEquation);
+  $('#clear').on('click', clearCalculator);
+} // end onReady
 
 function changeOperator() {
   console.log('in changeOp', this.value);
   operator = this.value;
-}
+} // end changeOperator
 
-function doCalculation(event) {
+function sendEquation(event) {
   event.preventDefault();
-  console.log('in doCalc', operator);
-}
+  console.log('in sendEq');
+
+  //make sure an operator is selected
+  if (!operator || $('#num1').val('') || $('#num2').val('')) {
+    console.log('Missing inputs!');
+  } else {
+    console.log('we are good to go');
+    // create new equation object
+    const newEquation = {
+      num1: Number($('#num1').val()),
+      num2: Number($('#num2').val()),
+      operator: operator,
+    };
+    console.log(newEquation);
+    // send it to the server
+    clearCalculator();
+  }
+} // end sendEquation
+
+function clearCalculator() {
+  $('#num1').val('');
+  $('#num2').val('');
+  operator = '';
+} // end clearCalc
